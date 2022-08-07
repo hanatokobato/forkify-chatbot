cadbffb1c25a22c003efba9bd72b5f8847c02e0d
bangld <kobato2096@gmail.com>
2022-08-07 23:40:51 +0700

    temp

--- bot/controllers/bot_controller.rb
+++ bot/controllers/bot_controller.rb
@@ -3,6 +3,12 @@ class BotController < Stealth::Controller
   helper :all
 
   def route
+    if current_message.payload.present?
+      handle_payloads
+      current_message.payload = nil
+      return
+    end
+
     if current_session.present?
       step_to session: current_session
     else
@@ -10,4 +16,16 @@ class BotController < Stealth::Controller
     end
   end
 
+  private
+
+  def handle_payloads
+    payload = current_message.payload
+    if payload == 'new_user' || payload == 'developer_restart'
+      step_to flow: 'hello', state: 'say_hello'
+    elsif payload == 'restart pressed'
+      step_to flow: 'goodbye', state: 'say_hello_again'
+    else
+      return
+    end
+  end
 end
